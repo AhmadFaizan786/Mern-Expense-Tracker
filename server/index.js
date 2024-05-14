bad1ef651f96e1e22d2c8ff7ff2b99d2869f9070
Faizan Ahmad <fa371250@gmail.com>
2024-05-14 23:29:44 +0530

    commits

--- server/index.js
+++ server/index.js
@@ -8,11 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/api/test',(req,res)=>{
-  res.json({body:'test ok'})
-})
-
 app.post('/api/transaction',async(req,res)=>{
   // console.log(process.env.MONGO_URL)
   await mongoose.connect(process.env.MONGO_URL)
@@ -28,4 +23,8 @@ app.get('/api/transactions',async (req,res)=>{
 })
 
 
-app.listen({port:3030})
+const basePath = process.env.BASE_PATH || '';
+const PORT = process.env.PORT ||3030
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
